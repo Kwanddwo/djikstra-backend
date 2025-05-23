bf3740099fc2224c1e2cc2c142e61bec11d4f5eb
Marouane <marouanelemghari@gmail.com>
2025-05-23 15:30:34 +0100

    fix: protocol change

--- db/db.py
+++ db/db.py
@@ -8,6 +8,10 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./djikstra.db")
 
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
